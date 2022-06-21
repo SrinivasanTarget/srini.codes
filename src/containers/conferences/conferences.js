28c9782124856438cbf7b1189da090116277ad0a
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2022-06-21 19:35:17 +0530

    redesign conference cards

--- src/containers/conferences/conferences.js
+++ src/containers/conferences/conferences.js
@@ -24,55 +24,59 @@ export default function Conferences() {
         {conferences.slice(0, 2).map((conference, i) => {
           return (
             <div key={i} className="col-span-full lg:col-span-6">
-              <div className="bg-secondary text-primary flex h-full w-full flex-col justify-between rounded-lg p-16 pt-20 bg-gray-800 text-white hover:bg-gradient-to-r from-gray-700 via-gray-900 to-black">
-                <div>
-                  <div className="-mr-4 mb-12 flex flex-wrap">
-                    {conference.tags.map((tag, i) => {
-                      return (
-                        <div
-                          key={i}
-                          className="text-primary mb-4 mr-4 rounded-full px-6 py-1 bg-gray-700"
-                        >
-                          {tag}
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <span className="max-w-full text-lg text-secondary mb-10 pb-7 prose text-gray-400">
-                    {conference.title}
-                  </span>
-                  <h3 className="text-2xl md:text-3xl mb-5 mt-5">
-                    {conference.description}
-                  </h3>
-                </div>
-                <a
-                  className="mt-16 text-primary inline-flex items-center text-left font-lora focus:outline-none cursor-pointer transition"
-                  href={conference.url}
-                >
-                  <span className="text-xl font-medium">
-                    <span className="hidden md:inline">check this talk</span>
-                    <span className="md:hidden">Read more</span>
-                  </span>
-                  <div className="relative inline-flex h-14 w-14 flex-none items-center justify-center p-1">
-                    <div className="absolute text-gray-200">
-                      <svg
-                        className="transform rotate-[270deg] text-gray-400"
-                        width="40"
-                        height="40"
-                        viewBox="0 0 32 32"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M15.101 5.5V23.1094L9.40108 17.4095L8.14807 18.6619L15.9862 26.5L23.852 18.6342L22.5996 17.3817L16.8725 23.1094V5.5H15.101Z"
-                          fill="currentColor"
-                        ></path>
-                      </svg>
+              <div className="p-0.5 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-secondary text-primary flex h-full w-full flex-col justify-between rounded-xl text-white">
+                <div className="p-0.5 bg-gray-800 rounded-xl">
+                  <div className="p-16">
+                    <div className="-mr-4 mb-12 flex flex-wrap">
+                      {conference.tags.map((tag, i) => {
+                        return (
+                          <div
+                            key={i}
+                            className="text-primary mb-4 mr-4 rounded-full px-6 py-1 bg-gradient-to-r from-gray-800 via-gray-900 to-black"
+                          >
+                            {tag}
+                          </div>
+                        );
+                      })}
                     </div>
+                    <span className="max-w-full text-lg text-secondary mb-10 pb-7 prose text-gray-400">
+                      {conference.title}
+                    </span>
+                    <h3 className="text-2xl md:text-3xl mb-5 mt-5">
+                      {conference.description}
+                    </h3>
                   </div>
-                </a>
+                  <a
+                    className="text-primary inline-flex items-center text-left font-lora focus:outline-none cursor-pointer transition"
+                    href={conference.url}
+                  >
+                    <span className="text-xl font-medium">
+                      <span className="hidden md:inline pl-16">
+                        check this talk
+                      </span>
+                      <span className="md:hidden pl-16">Read more</span>
+                    </span>
+                    <div className="relative inline-flex h-14 w-14 flex-none items-center justify-center p-1">
+                      <div className="absolute text-gray-200">
+                        <svg
+                          className="transform rotate-[270deg] text-gray-400"
+                          width="40"
+                          height="40"
+                          viewBox="0 0 32 32"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M15.101 5.5V23.1094L9.40108 17.4095L8.14807 18.6619L15.9862 26.5L23.852 18.6342L22.5996 17.3817L16.8725 23.1094V5.5H15.101Z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                  </a>
+                </div>
               </div>
             </div>
           );
