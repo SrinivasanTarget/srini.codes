0a2adb161637b24fa665bb06a5433ac0d44a8fc1
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2022-06-29 23:47:33 +0530

    move to nextgen image sourcesets

--- src/portfolio/projects.js
+++ src/portfolio/projects.js
@@ -4,54 +4,54 @@ const projects = [
     description:
       "cross-platform test automation tool for native, hybrid, and mobile web and desktop apps.",
     source: "https://github.com/appium/java-client",
-    imgSource: "appium.png",
+    imgSource: "appium.webp",
   },
   {
     title: "Selenium",
     description:
       "Browser automation tool that can be used to test web applications.",
     source: "https://github.com/SeleniumHQ/selenium",
-    imgSource: "selenium.png",
+    imgSource: "selenium.webp",
   },
   {
     title: "webdriverio",
     description:
       "WebdriverIO is a test automation framework that allows you to run tests based on the Webdriver protocol and Appium automation technology.",
     source: "https://github.com/webdriverio/webdriverio",
-    imgSource: "webdriverio.png",
+    imgSource: "webdriverio.webp",
   },
   {
     title: "Appium Test Distribution",
     description:
       "A tool for running android and iOS appium tests in parallel across devices.",
     source: "https://github.com/AppiumTestDistribution/AppiumTestDistribution",
-    imgSource: "ATD.png",
+    imgSource: "ATD.webp",
   },
   {
     title: "Appium Device Farm",
     description:
       "Appium 2.0 plugin designed to manage and create driver sessions on available devices.",
     source: "https://github.com/AppiumTestDistribution/appium-device-farm",
-    imgSource: "ATD.png",
+    imgSource: "ATD.webp",
   },
   {
     title: "Appium Wait Plugin",
     description: "Appium plugin designed to wait for element to be present.",
     source: "https://github.com/AppiumTestDistribution/appium-wait-plugin",
-    imgSource: "ATD.png",
+    imgSource: "ATD.webp",
   },
   {
     title: "Appium Gestures Plugin",
     description:
       "Appium plugin designed to perform gestures using W3C Actions.",
     source: "https://github.com/AppiumTestDistribution/appium-gestures-plugin",
-    imgSource: "ATD.png",
+    imgSource: "ATD.webp",
   },
   {
     title: "React Testing Library",
     description: "Light-weight solution for testing React components.",
     source: "https://github.com/testing-library/react-testing-library",
-    imgSource: "ReactTestingLibrary.png",
+    imgSource: "ReactTestingLibrary.webp",
   },
 ];
 
