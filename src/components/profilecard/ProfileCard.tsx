2527ba4d0899f84f3bf53e848582ec2bf521800d
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2022-09-04 13:22:40 +0530

    lazy load images

--- src/components/profilecard/ProfileCard.tsx
+++ src/components/profilecard/ProfileCard.tsx
@@ -3,6 +3,7 @@ export default function ProfileCard() {
     <div className='col-span-4 col-start-9 row-span-2 row-start-1 mt-0 hidden xl:block'>
       <img
         decoding='async'
+        loading='lazy'
         className='max-h-50vh rounded-br-[25%] rounded-tl-[25%] rounded-bl-3xl rounded-tr-3xl'
         src={new URL(`../../assets/images/SrinivasanSekar.webp`, import.meta.url).href}
         alt='Srinivasan Sekar'
