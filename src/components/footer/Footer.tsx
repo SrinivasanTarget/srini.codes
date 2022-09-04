85dbff52416a857d614ce7324f95356184db1a47
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2022-09-04 12:17:31 +0530

    fix accessibility issues

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='md:container md:mx-auto pt-5' id='blogs'>
-      <h3 className='text-indigo-100 text-sm decoration-4 p-4'>
-        <p className='text-indigo-100 text-center'>© Srinivasan Sekar</p>
-      </h3>
+      <p className='text-indigo-100 text-sm decoration-4 p-4 text-indigo-100 text-center'>
+        © Srinivasan Sekar
+      </p>
     </div>
   )
 }
